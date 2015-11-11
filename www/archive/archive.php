22f7d094627b04ebd40d1b39b6abe485acd20fc7
Nick Mattei <nsmattei@gmail.com>
2015-11-11 13:28:09 +1100

    archive page

---
+++ www/archive/archive.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<?php include "./common/head.php"; ?>
+</head>
+
+<body>
+	<div class="container_12">
+		<?php include "./common/menu.php"; ?>
+	
+		<!-- Site Archive -->
+		<div class="grid_7">
+			<h5> Site Archive </h5>
+			<p> Previous editions of PrefLib.  These archives contain a full, compressed copy of the Library and website for the given release date.</p>
+			<ul>
+				<li class="wide"> <a href="./archive/PrefLib_v1.6.zip">PrefLib Version 1.6.</a> Stable Release: October 2, 2014.  Major site update and bug fixes.</li>
+				<li class="wide"><a href="./archive/PrefLib_v1.0.zip">PrefLib Version 1.0.</a> Stable Release: December 3, 2013; Revision 164. Major update to the database and first tools release.</li>
+				<li class="wide"><a href="./archive/PrefLib_v0.5.zip">PrefLib Version 0.5.</a> Stable Release: June 4, 2013; Revision 55. First version of the library.</li>
+			</ul>
+		</div>
+
+		<!-- Pictures and Links -->
+		<div class="grid_5">
+			<p>
+			<img src="images/pref.png" alt="" title="" />
+
+			<a href="http://www.nicta.com.au/category/research/optimisation/" class="center"> <h3>Supported By:</h3><img src="images/nicta.png" alt="" title="NICTA" /></a>
+			</p>
+		</div>
+
+
+		<!-- Break Page... -->
+		<div class="clear"></div>
+		<div class="grid_12 spacer"></div>
+		<div class="clear"></div>
+
+		<!-- News Archive -->
+		<div class="grid_8">
+			<h5> News Archive </h5>
+			<div class="news_box">
+			<div class="news_icon"></div>
+		    	<div class="news_content">
+		        	<b>June 1, 2015:</b>
+		            <p> We have moved the devlopment and hosting of <a href="https://github.com/nmattei/PrefLib-Tools" target="_blank"> PrefLib Tools over to GitHub!</a>  Check out the current version and join us on expanding the capabilities of PrefLib!</p>
+		        	</div>
+		    </div>
+
+			<div class="news_box">
+			<div class="news_icon"></div>
+		       	<div class="news_content">
+		            <b>Jan. 15, 2015:</b>
+		            <p> We are hosting the 2nd workshop on <a href="http://www.explore-2015.preflib.org/">Exploring Beyond the Worst Case in Computational Social Choice</a> at <a href="http://www.aamas2015.com/en/">AAMAS 2015</a>.  Please check our workshop webpage for more details!</p>
+		        </div>
+		    </div>
+			
+			<div class="news_box">
+			<div class="news_icon"></div>
+		        <div class="news_content">
+		            <b>Oct. 20, 2014:</b>
+		            <p> Release of PrefLib v. 1.6, incorporating a substantial update of the library with 5 new data sources, many thanks to <a href="https://sites.google.com/site/jflaslierhomepage/">J-F Laslier</a>. There have been a few small bug fixes files from data sets ED-00015 and ED-00018.</p>
+		                	
+		            <p> There is also a significant update to the <a href="tools.php">Tools page.</a>We now have two community donated tool chains, many thanks to <a href="http://jpdickerson.com/">John Dickerson</a> and <a href="http://www.cs.huji.ac.il/~omerl/"> Omer Lev</a>.</p>
+		        </div>
+		    </div>
+
+			<div class="news_box">
+		    <div class="news_icon"></div>
+		        <div class="news_content">
+		            <b>June 20, 2014:</b>
+		            <p> Massive site update sees a revamp of the top menu and the addition of the <a href="archive.php">Archive</a> page.</p>
+
+					<p> We have added over 1,000 new data sets from over 10 new sources -- bringing the total library size to over 4,000 files! We have also revamped the <a href="source.php">data pages</a> to make them easier to navigate. A small change to the <a href="format.php">file extensions</a> has been made for more clarity.</p>
+		                	
+		            <p> Finally there is a small update to the <a href="tools.php">Tools</a> library inclding a command line interface and functions to test for Single Peakedness.</p>
+		        </div>   
+		    </div>
+			
+			<div class="news_box">
+	        <div class="news_icon"></div>
+            	<div class="news_content">
+                	<b>Dec. 10, 2013:</b>
+                	<p> We are hosting a workshop at <a href="http://aamas2014.lip6.fr/">AAMAS 2014</a> on <a href="http://www.explore14.preflib.org/">Exploring Beyond the Worst Case in Computational Social Choice</a>.  Nick will give a talk about PrefLib!  Please consider joining us in Paris in the coming year.</p>
+                </div>   
+	    	</div>
+
+			<div class="news_box">
+	        <div class="news_icon"></div>
+            	<div class="news_content">
+                	<b>Nov. 6, 2013:</b>
+                	<p> The first release of the tool suite is now available on the <a href="tools.php">Tools</a> page. Python3 scripts to read, write, and generate preference data in our formats!</p>
+                </div>   
+	    	</div>
+
+			<div class="news_box">
+	        <div class="news_icon"></div>
+            	<div class="news_content">
+                	<b>Sept. 3, 2013:</b><p> A big update today brings us over 3000 datasets hosted on the site with a full data archive over 7 GB!</p> <p> We have also added a <a href="about.php">Thanks!!</a> section to recognize those individuals who have helped make PrefLib possible.</p>
+                </div>
+	        </div>	
+	        
+			<div class="news_box">
+	        <div class="news_icon"></div>
+	            <div class="news_content">
+	                <b>July 1, 2013:</b>
+	                <p> Our paper has been accepted to <a href="http://www.adt2013.org/">2013 Conference on Algorithmic Decision Theory</a>.  We have also had several new donated datasets which have been parsed and posted. <br> </br> We have added a new <a href="papers.php">Papers</a> section to the site with a list of papers that have used PrefLib!</p>
+	            </div>   
+	        </div>
+
+			<div class="news_box">
+        	<div class="news_icon"></div>
+            	<div class="news_content">
+	                	<b>April 1, 2013:</b>
+	                	<p> Major site redesign and addition of many new datasets.</p>
+                </div>   
+	        </div>
+
+			<div class="news_box">
+        	<div class="news_icon"></div>
+            	<div class="news_content">
+	                	<b>March 15, 2013:</b>
+	                	<p> Initial Version Established</p>
+                </div>   
+	        </div>
+		</div>
+		
+		<!-- Links -->
+		<div class="grid_4">
+			<h5 style=text-align:center> Links </h5>
+			<?php include "./common/links.php"; ?>
+		</div>
+	
+		<!-- Break and Footer -->
+		<?php include "./common/foot.php"; ?>	
+	</div>
+</body>
+</html>
