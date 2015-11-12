cca76212641df2d4bf002f666d65421e1d7ade6d
Nick Mattei <nsmattei@gmail.com>
2015-11-12 16:17:12 +1100

    working synch script

--- www/scripts/sync_dreamhost.py
+++ www/scripts/sync_dreamhost.py
@@ -58,16 +58,9 @@ if __name__ == '__main__':
 		'''
 		A wrapper around system calls to build the PrefLib webiste.
 		Requires that you have SSH Key access to the server at www.preflib.org and www.dev.preflib.org.
-		'''
-
-	parser.add_argument('-rb', '--rebuild', type=bool, default=False, dest='rebuild' help=\
-																	'''
-																	Rebuild the index before doing anything.
-																	''')
-	parser.add_argument('-m', '--main', type=bool, default=False, dest='main' help=\
-																	'''
-																	Push to the main site.
-																	''')
+		''')
+	parser.add_argument('-rb', '--rebuild', type=bool, default=False, dest='rebuild', help= '''Rebuild the index before doing anything.''')
+	parser.add_argument('-m', '--main', type=bool, default=False, dest='main', help='''Push to the main site.''')
 	results = parser.parse_args()
 
 	if results.rebuild:
@@ -75,93 +68,10 @@ if __name__ == '__main__':
 		os.system("./python3 build_data_html.py")
 
 	# Sync EVERYTHING to DEV.
-	rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" /Users/Nick/repo/github/PrefLib-www.git/www/ koolkamel@www.preflib.org:~/dev.preflib.org''')
+	print(" *** Pushing ENTIRE www directory to DEV@DREAMHOST *** ")
+	os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" /Users/Nick/repo/github/PrefLib-www.git/www/ koolkamel@www.preflib.org:~/dev.preflib.org''')
 
 
-	# Experiment Number
-	parser.add_argument('-e', '--experiment', type=int, dest='experiment', \
-		required=True, help="Experiment number to run. \n \
-		(1) Time Samples ApproConcorde \n \
-		(2) Time Samples SubsetConcorde \n \
-		(3) Time Shapley Computation \n \
-		(4) Error For Sample Number ApproConcorde\n \
-		(5) Error For Sample Number SubsetConcorde\n ")
-
-	# Number of locations to sweep.
-	parser.add_argument('-ml', '--min_locations', type=int, dest='min_locations', default=2, help='Minimum number of agents in settings.')
-	parser.add_argument('-Ml', '--max_locations', required=True, type=int, dest='max_locations', help='Maximum number of agents in settings.')
-
-	# Seed value
-	parser.add_argument('-sd', '--seed', type=int, default=-1, dest='random_seed', help='Random Seed, Default is time.')
-
-	# Tracking results.
-	parser.add_argument('-sa', '--samples', default=1000, type=int, dest='num_samples', help='Number of samples to run.')	
-	parser.add_argument('-f', '--frequency', default=1, type=int, dest='write_frequency', help='Frequency with which to record timing/error samples.')
-	parser.add_argument('-r', '--runs', default=1, type=int, dest='num_runs', help='Numer of times to repeat experiment step for jitter.')
-
-	results = parser.parse_args()
-
-
-	if(len(sys.argv) != 3):
-		print("Use: " + str(sys.argv[0]) + " <-w (Website Only) | -d (Data Only) | -a (All)> <-m (Main) | -d (Dev)>\n")
-		sys.exit()
-		
-	#Parse Args...
-	switch = sys.argv[1]
-	site = sys.argv[2]
-	
-	#Run the local commands in this order:
-	# print("**** Updating Individual Data Pages **** \n\n")
-	# os.system("python3 ./makedatapages.py")
-	
-	# print("**** Updating Data Packs **** \n\n")
-	# os.system("python3 ./makedatapacks.py")
-	
-	# print("**** Updating Domain Page **** \n\n")
-	# os.system("python3 ./makedomainpage.py")
-	
-	# print("**** Updating Public Link Pages **** \n\n")
-	# os.system("python3 ./makepublinkpage.py")
-	
-	# c = input("\n\n\**** Go ADD the new stuff (if needed) in the GIT directories for WWW as it is automatically pushed for DEV!. ****")
-	
-	# Post Recieve hooks on data/www for Git auto-exports to the DEV site only.
-
-	#If we got -a do a hard refresh of the www, data, and archive folder.
-	if site == "-d" and switch == "-a":
-		print("**** Pushing with --delete-after www and data directories to DEV ****")
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="data/*" --exclude="/archive/*" --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/repo/preflib/www/ koolkamel@www.preflib.org:~/dev.preflib.org''')
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/repo/preflib/database/ koolkamel@www.preflib.org:~/dev.preflib.org/data''')
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/repo/preflib/archive/ koolkamel@www.preflib.org:~/dev.preflib.org/archive''')
-
-	#Hard refresh of the www directory.
-	if site == "-d" and switch =="-w":
-		print("**** Pushing WWW with --delete-after option to DEV ****")
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="data/*" --exclude="/archive/*" --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/repo/preflib/www/ koolkamel@www.preflib.org:~/dev.preflib.org''')
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/repo/preflib/archive/ koolkamel@www.preflib.org:~/dev.preflib.org/archive''')
-
-	# If we are updating data on the dev site need to run the RSYNC:
-	elif site == "-d" and switch == "-d":
-		print("**** Pushing Current Data Directory to DEV ****")
-		#try running with --delete-after to clean out extraneous files...
-		#rsync -azvP --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" --delete-after ./data-PrefLib.git/ koolkamel@www.preflib.org:~/dev.preflib.org/data
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/Repo/PrefLib/data-PrefLib.git/ koolkamel@www.preflib.org:~/dev.preflib.org/data''')
-
-
-	# For Main Site we need to make sure the ARCHIVE folder is synced and that we don't take the .htaccess folder from the www directory.
-	elif site == "-m" and switch == "-a":
-		print("**** Pushing with --delete-after www, data, and archive to MAIN ****")
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="beyond2014/" --exclude="data/*" --exclude="/archive/*" --exclude=".htaccess" --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/Repo/PrefLib/www-PrefLib.git/ koolkamel@www.preflib.org:~/preflib.org''')
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/Repo/PrefLib/data-PrefLib.git/ koolkamel@www.preflib.org:~/preflib.org/data''')
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/Repo/PrefLib/archive/ koolkamel@www.preflib.org:~/preflib.org/archive''')
-
-	# For Main Site don't take the .htaccess folder with you.
-	elif site == "-m" and switch == "-w":
-		print("**** Pushing with --delete-after www to MAIN ****")
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="beyond2014/" --exclude="data/*" --exclude="/archive/*" --exclude=".htaccess" --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/Repo/PrefLib/www-PrefLib.git/ koolkamel@www.preflib.org:~/preflib.org''')
-
-	# For Main Site we need to make sure the ARCHIVE folder is synced and that we don't take the .htaccess folder from the www directory.
-	elif site == "-m" and switch == "-d":
-		print("**** Pushing with --delete-after data, and archive to MAIN ****")
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/Repo/PrefLib/data-PrefLib.git/ koolkamel@www.preflib.org:~/preflib.org/data''')
-		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".git/" --exclude=".gitignore" /Users/Nick/Repo/PrefLib/archive/ koolkamel@www.preflib.org:~/preflib.org/archive''')
+	# Do a main push but don't take the .htaccess and .htpassword
+	if results.main:
+		os.system('''rsync --recursive --times --verbose --progress --compress --delete-after --exclude="*.DS_Store" --exclude=".htaccess" --exclude=".htpasswd" /Users/Nick/repo/github/PrefLib-www.git/www/ koolkamel@www.preflib.org:~/www.preflib.org''')
