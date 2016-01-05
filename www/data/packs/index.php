4d32519d57d3207a209b685680b0b5b8a28d3861
Nick Mac <nsmattei@gmail.com>
2016-01-05 17:09:12 +1100

    Fixed issue with Sushi Dataset where I had been told the wrong file formats.

--- www/data/packs/index.php
+++ www/data/packs/index.php
@@ -77,7 +77,7 @@ For information on the types of data on this site, please see <a href="/data/for
 
 <div class="news_box"><a name="mjg"></a><h5>Majority Graph</h5>
 <p>
-We currently have 314 data files with an <b>mjg</b> extension on the site. 
+We currently have 313 data files with an <b>mjg</b> extension on the site. 
 <a href="/data/packs/mjg.zip" onClick="_gaq.push(['_trackEvent', 'Download', 'pack',  + ext + ]);">Click Here To Download.</a> <br><br>
 For information on the types of data on this site, please see <a href="/data/format.php">Data Formats</a>.</p></div>
 <br>
