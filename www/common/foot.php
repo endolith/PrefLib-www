f935f10ab41f30875b3e096640062f494ae5efee
Nick Mac <nsmattei@gmail.com>
2016-08-12 14:09:55 +1000

    san sebastian directory

--- www/common/foot.php
+++ www/common/foot.php
@@ -8,9 +8,9 @@
 		PrefLib is supported by <a href="http://www.nicta.com.au">NICTA</a>. PrefLib was founded and is maintained by <a href="http://www.nickmattei.net">Nicholas Mattei</a> and <a href="http://www.cse.unsw.edu.au/~tw/">Toby Walsh</a>
 	</div>
 	<div class="grid_4">
-		We rely on the support of the community in order to grow the usefulness of this site.  To contribute, please contact <a href="http://www.nickmattei.net">Nicholas Mattei</a> at: <h6>nicholas<b>{DOT}</b>mattei@nicta.com.au</h6>
+		We rely on the support of the community in order to grow the usefulness of this site.  To contribute, please contact <a href="http://www.nickmattei.net">Nicholas Mattei</a> at: <h6>nsmattei<b>{AT}</b>gmail</h6>
 	</div>
 	<div class="grid_4 omega"></div>
 </div>
 
-<div class="clear"></div>	
+<div class="clear"></div>
---
+++ www/data/election/poster/index.php
@@ -0,0 +1,78 @@
+
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/common/head.php'); ?>
+	</head>
+
+	<body>
+		<div class="container_12">
+			<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/common/menu.php'); ?>
+	
+
+&nbsp;<div class="grid_7">
+<h5>ED-00033: San Sebastian Poster Competition</h5>
+<p>Approval Ballots from the San Sebastian Poster Competition held during <a href="http://www.illc.uva.nl/COST-IC1205/Events/Action-Summer-Schools/San-Sebastian-2016/">The Summer School on Computational Social Choice</a> organised by COST Action IC1205 at the Miramar Palace in San Sebastian in July 2016.  This set has two elections of approval ballots with 17 candidates and about 60 voters each.</p><p>Two elections were held, using approval voting. In the first election the alternatives were posters A1-A17; in the second election the alternatives were posters B1-B17. There were 67 eligible voters (56 summer school participants, including the 34 poster presenters, as well as 7 lecturers and 4 organisers). Of these, 65 voters participated in the first election and 60 voters participated in the second election (1 voter did not vote in either election). The elections were conducted using the WHALE system of Sylvain Bouveret.</p><p> The origional data file (ED-00033-00000001.dat) includes one column per poster. Each of the two sets of posters is ordered by the number of approvals received. Each row corresponds to a voter. The voters are ordered by the number of approvals they have given across both elections, except that the 7 voters who only participated in one of the two elections are listed last. Most of the posters are available at the summer school website.  The other files are convereted into standard PrefLib format where all approved candidates are considered a tied equivlance class.</p>
+
+<p><h6>Required Citations</h6><p><ul> 
+<li>None.</li></ul></p>
+
+<p><h6>Selected Citations Using This Dataset</h6><p><ul> 
+<li>None.</li></ul></p>
+
+</div>
+			<!-- Pictures and Links -->
+			<div class="grid_5">
+				<p>
+				<img src="/images/pref.png" alt="" title="" />
+
+				<a href="http://www.nicta.com.au/category/research/optimisation/" class="center"> <h3>Supported By:</h3><img src="/images/nicta.png" alt="" title="NICTA" /></a>
+				</p>
+			</div>
+			
+			<!-- Break Page... -->
+			<div class="clear"></div>
+			<div class="grid_12 spacer"></div>
+			<div class="clear"></div>
+	
+
+<div class="grid_12"><p><ul><h5 style="text-align:center;"><a href=" /data/election/poster/poster.zip ">Download This Complete Dataset (8.0K).</a></h5></ul></p></div>
+
+<!-- Break Page... -->
+																		<div class="clear"></div>
+																		<div class="grid_12 spacer"></div>
+																		<div class="clear"></div>
+														 
+																		<div class="grid_12">
+																		<table class="DataTable">
+																		<tr><th>Description</th><th>Type</th><th>Modification</th><th>File Name</th><th>File Size</th></tr>
+
+																		 <tr><td> San_Sebastian_All_Approvals </td><td> <a href="/data/format.php#data">Extra Data File (CSV)</a></td> <td> Original </td> <td> <a href="/data/election/poster/ED-00033-00000001.dat" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000001.dat']);">ED-00033-00000001.dat</a> </td><td> 8.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_1 </td><td> <a href="/data/format.php#toc">Order with Ties - Complete List</a></td> <td> Original </td> <td> <a href="/data/election/poster/ED-00033-00000002.toc" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000002.toc']);">ED-00033-00000002.toc</a> </td><td> 4.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_1 </td><td> <a href="/data/format.php#pwg">Pairwise Graph</a></td> <td> Induced </td> <td> <a href="/data/election/poster/ED-00033-00000002.pwg" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000002.pwg']);">ED-00033-00000002.pwg</a> </td><td> 4.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_1 </td><td> <a href="/data/format.php#wmg">Weighted Majority Graph</a></td> <td> Induced </td> <td> <a href="/data/election/poster/ED-00033-00000002.wmg" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000002.wmg']);">ED-00033-00000002.wmg</a> </td><td> 4.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_1 </td><td> <a href="/data/format.php#tog">Tournament Graph</a></td> <td> Induced </td> <td> <a href="/data/election/poster/ED-00033-00000002.tog" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000002.tog']);">ED-00033-00000002.tog</a> </td><td> 4.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_2 </td><td> <a href="/data/format.php#toc">Order with Ties - Complete List</a></td> <td> Original </td> <td> <a href="/data/election/poster/ED-00033-00000003.toc" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000003.toc']);">ED-00033-00000003.toc</a> </td><td> 4.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_2 </td><td> <a href="/data/format.php#pwg">Pairwise Graph</a></td> <td> Induced </td> <td> <a href="/data/election/poster/ED-00033-00000003.pwg" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000003.pwg']);">ED-00033-00000003.pwg</a> </td><td> 4.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_2 </td><td> <a href="/data/format.php#wmg">Weighted Majority Graph</a></td> <td> Induced </td> <td> <a href="/data/election/poster/ED-00033-00000003.wmg" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000003.wmg']);">ED-00033-00000003.wmg</a> </td><td> 4.0K</td></tr> 
+
+ <tr><td> San_Sebastian_Group_2 </td><td> <a href="/data/format.php#tog">Tournament Graph</a></td> <td> Induced </td> <td> <a href="/data/election/poster/ED-00033-00000003.tog" onClick="_gaq.push(['_trackEvent', 'Download', 'individual', 'ED-00033-00000003.tog']);">ED-00033-00000003.tog</a> </td><td> 4.0K</td></tr> 
+
+
+		</table>
+		</div>
+
+
+			<!-- Break and Footer -->
+			<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/common/foot.php'); ?>	
+		</div>
+	</body>
+	</html>
+	
